61348ac20626a38c5eb639703aaf914669a4617e
Atothegod <Phudit.pre@dome.tu.ac.th.com>
2025-05-07 15:22:18 +0700

    Update Shopping Cart System

--- myproject/app1/admin.py
+++ myproject/app1/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from .models import Product
-
+from django.contrib.auth.models import User
 # Register your models here.
 from django.contrib import admin
 from .models import *
 
 models = [Admin, Analytic, CarbonFootprint, Cart, Chat, Order, Payment, Product,
-          Recommendation, Refund, Review, Seller, Shipping, Support, User]
+          Recommendation, Refund, Review, Seller, Shipping, Support]
 
 for model in models:
     admin.site.register(model)
--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, ProductDetailView, cart_view
 
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage'),
     path('products/', ProductListView.as_view(), name='product_list'),
     path('products/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
+    path('cart/', views.cart_view, name='cart_view'),
+    path('cart/remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
