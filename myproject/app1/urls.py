7fdc7e48c547498ba52145ac731086b10d75e945
Atothegod <Phudit.pre@dome.tu.ac.th>
2025-05-08 01:19:44 +0700

    Log in with google(option) modified

--- myproject/app1/urls.py
+++ myproject/app1/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
-from .views import ProductListView, ProductDetailView, cart_view
+from .views import ProductListView, ProductDetailView, cart_view, CustomLoginView
 from django.contrib.auth import views as auth_views
 
 
@@ -11,7 +11,8 @@ urlpatterns = [
     path('cart/add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart_view'),
     path('cart/remove/<int:cart_item_id>/', views.remove_from_cart, name='remove_from_cart'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signup/', views.signup_view, name='sign_up'),
+    path('auth/', include('social_django.urls', namespace='social')),
 ]
